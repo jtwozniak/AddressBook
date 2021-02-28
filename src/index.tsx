166d3a739fa06d8b68895cd458b15ef84e5e123d
Janusz <jtwozniak@gmail.com>
2021-02-28 13:31:41 +0000

    feat: progress with setting up webpack and typescript

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import * as ReactDOM from "react-dom"
+import { App } from "./App"
+
+ReactDOM.render(
+  <App userName="Beveloper" lang="TypeScript" />,
+  document.getElementById("output")
+)
